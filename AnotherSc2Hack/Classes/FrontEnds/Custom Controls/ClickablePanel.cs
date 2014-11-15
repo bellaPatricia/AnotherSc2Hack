583fa4f99f5981091d61aff4db4c7b7043d13094
bellapatricia <bpatriciaella@yahoo.com>
2014-11-15 20:36:28 +0100

    Some more GUI changes

--- AnotherSc2Hack/Classes/FrontEnds/Custom Controls/ClickablePanel.cs
+++ AnotherSc2Hack/Classes/FrontEnds/Custom Controls/ClickablePanel.cs
@@ -11,9 +11,77 @@ namespace AnotherSc2Hack.Classes.FrontEnds.Custom_Controls
     public class ClickablePanel : Panel
     {
         private Boolean _isClicked = false;
-        public Boolean IsClicked { get { return _isClicked; } set { _isClicked = value;
-            Invalidate();
-        } }
+
+        public Boolean IsClicked
+        {
+            get { return _isClicked; }
+            set
+            {
+                _isClicked = value;
+
+                if (value)
+                {
+                    foreach (var panel in Parent.Controls)
+                    {
+                        if (panel == this)
+                        {
+                            DisplayColor = _activeForegroundColor;
+                            continue;
+                        }
+
+                        if (panel.GetType() == typeof (ClickablePanel))
+                        {
+                            ((ClickablePanel) panel).IsClicked = false;
+                            ((ClickablePanel) panel).DisplayColor = _inactiveForegroundColor;
+                        }
+                    }
+                }
+
+
+                Invalidate();
+            }
+        }
+
+        private String _displayText = String.Empty;
+
+        public String DisplayText
+        {
+            get { return _displayText; }
+            set
+            {
+                _displayText = value;
+                _lMainText.Text = _displayText;
+            }
+        }
+
+        private Color _displayColor = Color.Wheat;
+
+        public Color DisplayColor
+        {
+            get { return _displayColor; }
+            set
+            {
+                _displayColor = value;
+
+                if (_lMainText != null)
+                    _lMainText.ForeColor = _displayColor;
+            }
+        }
+
+        private float _textSize = 9f;
+
+        public float TextSize
+        {
+            get { return _textSize; }
+            set
+            {
+                _textSize = value;
+
+                if (_lMainText != null)
+                    _lMainText.Font = new Font(Font.Name, _textSize);
+            }
+        }
+
         public Boolean IsHovering { get; set; }
 
         private Color _inactiveBackgroundColor = Color.FromArgb(255, 66, 79, 90);
@@ -22,6 +90,7 @@ namespace AnotherSc2Hack.Classes.FrontEnds.Custom_Controls
         private Color _inactiveForegroundColor = Color.FromArgb(255, 193, 193, 193);
         private Color _activeForegroundColor = Color.FromArgb(255, 242, 242, 242);
         private Color _newBackgroundColor = Color.Wheat;
+        private Label _lMainText = new Label();
 
         private Brush _inactiveBackground = new SolidBrush(Color.FromArgb(255, 66, 79, 90));
         private Brush _activeBackground = new SolidBrush(Color.FromArgb(255, 52, 63, 72));
@@ -29,10 +98,11 @@ namespace AnotherSc2Hack.Classes.FrontEnds.Custom_Controls
         private Brush _activeForeground = new SolidBrush(Color.FromArgb(255, 242, 242, 242));
         private Brush _hoverBackground = new SolidBrush(Color.FromArgb(255, 94, 105, 114));
         private Brush _selectionColor = Brushes.Orange;
+        private Boolean _bInitCalled = false;
 
         public ClickablePanel()
         {
-            Init();
+           // Init();
         }
 
         private void Init()
@@ -44,22 +114,46 @@ namespace AnotherSc2Hack.Classes.FrontEnds.Custom_Controls
 
             BackColor = _inactiveBackgroundColor;
 
+            if (DisplayText.Length <= 0)
+                DisplayText = "[SampleText]";
+
+            TextSize = 13;
+            DisplayColor = _inactiveForegroundColor;
+
+            var fTextHeight = TextRenderer.MeasureText("dummy", _lMainText.Font).Height;
+            var fHeight = (float)Size.Height / 2;
+            var fY = fHeight - ((float)fTextHeight / 2);
+
+            _lMainText.Location = new Point(15, (int)fY);
+            _lMainText.MouseEnter += _lMainText_MouseEnter;
+            _lMainText.Click += _lMainText_Click;
             
+
+            Controls.Add(_lMainText);
+
+
+            _bInitCalled = true;
+        }
+
+        void _lMainText_Click(object sender, EventArgs e)
+        {
+            if (!IsClicked)
+                IsClicked = true;
+            
+        }
+
+        void _lMainText_MouseEnter(object sender, EventArgs e)
+        {
+            IsHovering = true;
         }
 
         protected override void OnMouseClick(MouseEventArgs e)
         {
             base.OnMouseClick(e);
 
-            foreach (var panel in Parent.Controls)
-            {
-                if (panel.GetType() == typeof(ClickablePanel))
-                {
-                    ((ClickablePanel)panel).IsClicked = false;
-                }
-            }
-
-            IsClicked = true;
+            
+            if (!IsClicked)
+                IsClicked = true;
         }
 
         protected override void OnMouseEnter(EventArgs e)
@@ -102,6 +196,14 @@ namespace AnotherSc2Hack.Classes.FrontEnds.Custom_Controls
             }
 
             BackColor = _newBackgroundColor;
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+
+            if (!_bInitCalled)
+                Init();
         }
     }
 }
--- AnotherSc2Hack/Classes/FrontEnds/MainHandler/NewMainHandler.Designer.cs
+++ AnotherSc2Hack/Classes/FrontEnds/MainHandler/NewMainHandler.Designer.cs
@@ -28,76 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlLeftSelection = new System.Windows.Forms.Panel();
+            this.pnlMainArea = new System.Windows.Forms.Panel();
+            this.clickablePanel1 = new AnotherSc2Hack.Classes.FrontEnds.Custom_Controls.ClickablePanel();
             this.clickablePanel5 = new AnotherSc2Hack.Classes.FrontEnds.Custom_Controls.ClickablePanel();
+            this.clickablePanel2 = new AnotherSc2Hack.Classes.FrontEnds.Custom_Controls.ClickablePanel();
             this.clickablePanel4 = new AnotherSc2Hack.Classes.FrontEnds.Custom_Controls.ClickablePanel();
             this.clickablePanel3 = new AnotherSc2Hack.Classes.FrontEnds.Custom_Controls.ClickablePanel();
-            this.clickablePanel2 = new AnotherSc2Hack.Classes.FrontEnds.Custom_Controls.ClickablePanel();
-            this.clickablePanel1 = new AnotherSc2Hack.Classes.FrontEnds.Custom_Controls.ClickablePanel();
+            this.pnlLeftSelection.SuspendLayout();
             this.SuspendLayout();
             // 
-            // clickablePanel5
+            // pnlLeftSelection
             // 
-            this.clickablePanel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(79)))), ((int)(((byte)(90)))));
-            this.clickablePanel5.IsClicked = false;
-            this.clickablePanel5.IsHovering = false;
-            this.clickablePanel5.Location = new System.Drawing.Point(50, 200);
-            this.clickablePanel5.Name = "clickablePanel5";
-            this.clickablePanel5.Size = new System.Drawing.Size(186, 50);
-            this.clickablePanel5.TabIndex = 4;
+            this.pnlLeftSelection.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pnlLeftSelection.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.pnlLeftSelection.Controls.Add(this.clickablePanel1);
+            this.pnlLeftSelection.Controls.Add(this.clickablePanel5);
+            this.pnlLeftSelection.Controls.Add(this.clickablePanel2);
+            this.pnlLeftSelection.Controls.Add(this.clickablePanel4);
+            this.pnlLeftSelection.Controls.Add(this.clickablePanel3);
+            this.pnlLeftSelection.Location = new System.Drawing.Point(0, 0);
+            this.pnlLeftSelection.Name = "pnlLeftSelection";
+            this.pnlLeftSelection.Size = new System.Drawing.Size(152, 386);
+            this.pnlLeftSelection.TabIndex = 5;
             // 
-            // clickablePanel4
+            // pnlMainArea
             // 
-            this.clickablePanel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(79)))), ((int)(((byte)(90)))));
-            this.clickablePanel4.IsClicked = false;
-            this.clickablePanel4.IsHovering = false;
-            this.clickablePanel4.Location = new System.Drawing.Point(50, 150);
-            this.clickablePanel4.Name = "clickablePanel4";
-            this.clickablePanel4.Size = new System.Drawing.Size(186, 50);
-            this.clickablePanel4.TabIndex = 3;
-            // 
-            // clickablePanel3
-            // 
-            this.clickablePanel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(79)))), ((int)(((byte)(90)))));
-            this.clickablePanel3.IsClicked = false;
-            this.clickablePanel3.IsHovering = false;
-            this.clickablePanel3.Location = new System.Drawing.Point(50, 100);
-            this.clickablePanel3.Name = "clickablePanel3";
-            this.clickablePanel3.Size = new System.Drawing.Size(186, 50);
-            this.clickablePanel3.TabIndex = 2;
-            // 
-            // clickablePanel2
-            // 
-            this.clickablePanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(79)))), ((int)(((byte)(90)))));
-            this.clickablePanel2.IsClicked = false;
-            this.clickablePanel2.IsHovering = false;
-            this.clickablePanel2.Location = new System.Drawing.Point(50, 50);
-            this.clickablePanel2.Name = "clickablePanel2";
-            this.clickablePanel2.Size = new System.Drawing.Size(186, 50);
-            this.clickablePanel2.TabIndex = 1;
+            this.pnlMainArea.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlMainArea.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.pnlMainArea.Location = new System.Drawing.Point(152, 0);
+            this.pnlMainArea.Name = "pnlMainArea";
+            this.pnlMainArea.Size = new System.Drawing.Size(699, 386);
+            this.pnlMainArea.TabIndex = 6;
             // 
             // clickablePanel1
             // 
             this.clickablePanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(79)))), ((int)(((byte)(90)))));
+            this.clickablePanel1.DisplayColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(193)))), ((int)(((byte)(193)))));
+            this.clickablePanel1.DisplayText = "Text";
             this.clickablePanel1.IsClicked = false;
             this.clickablePanel1.IsHovering = false;
-            this.clickablePanel1.Location = new System.Drawing.Point(50, 0);
+            this.clickablePanel1.Location = new System.Drawing.Point(0, 0);
             this.clickablePanel1.Name = "clickablePanel1";
-            this.clickablePanel1.Size = new System.Drawing.Size(186, 50);
+            this.clickablePanel1.Size = new System.Drawing.Size(152, 40);
             this.clickablePanel1.TabIndex = 0;
+            this.clickablePanel1.TextSize = 9F;
+            // 
+            // clickablePanel5
+            // 
+            this.clickablePanel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(79)))), ((int)(((byte)(90)))));
+            this.clickablePanel5.DisplayColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(193)))), ((int)(((byte)(193)))));
+            this.clickablePanel5.DisplayText = "[SmapleText]";
+            this.clickablePanel5.IsClicked = false;
+            this.clickablePanel5.IsHovering = false;
+            this.clickablePanel5.Location = new System.Drawing.Point(0, 160);
+            this.clickablePanel5.Name = "clickablePanel5";
+            this.clickablePanel5.Size = new System.Drawing.Size(152, 40);
+            this.clickablePanel5.TabIndex = 4;
+            this.clickablePanel5.TextSize = 9F;
+            // 
+            // clickablePanel2
+            // 
+            this.clickablePanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(79)))), ((int)(((byte)(90)))));
+            this.clickablePanel2.DisplayColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(193)))), ((int)(((byte)(193)))));
+            this.clickablePanel2.DisplayText = "[SmapleText]";
+            this.clickablePanel2.IsClicked = false;
+            this.clickablePanel2.IsHovering = false;
+            this.clickablePanel2.Location = new System.Drawing.Point(0, 40);
+            this.clickablePanel2.Name = "clickablePanel2";
+            this.clickablePanel2.Size = new System.Drawing.Size(152, 40);
+            this.clickablePanel2.TabIndex = 1;
+            this.clickablePanel2.TextSize = 9F;
+            // 
+            // clickablePanel4
+            // 
+            this.clickablePanel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(79)))), ((int)(((byte)(90)))));
+            this.clickablePanel4.DisplayColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(193)))), ((int)(((byte)(193)))));
+            this.clickablePanel4.DisplayText = "[SmapleText]";
+            this.clickablePanel4.IsClicked = false;
+            this.clickablePanel4.IsHovering = false;
+            this.clickablePanel4.Location = new System.Drawing.Point(0, 120);
+            this.clickablePanel4.Name = "clickablePanel4";
+            this.clickablePanel4.Size = new System.Drawing.Size(152, 40);
+            this.clickablePanel4.TabIndex = 3;
+            this.clickablePanel4.TextSize = 9F;
+            // 
+            // clickablePanel3
+            // 
+            this.clickablePanel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(79)))), ((int)(((byte)(90)))));
+            this.clickablePanel3.DisplayColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(193)))), ((int)(((byte)(193)))));
+            this.clickablePanel3.DisplayText = "[SmapleText]";
+            this.clickablePanel3.IsClicked = false;
+            this.clickablePanel3.IsHovering = false;
+            this.clickablePanel3.Location = new System.Drawing.Point(0, 80);
+            this.clickablePanel3.Name = "clickablePanel3";
+            this.clickablePanel3.Size = new System.Drawing.Size(152, 40);
+            this.clickablePanel3.TabIndex = 2;
+            this.clickablePanel3.TextSize = 9F;
             // 
             // NewMainHandler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(850, 385);
-            this.Controls.Add(this.clickablePanel5);
-            this.Controls.Add(this.clickablePanel4);
-            this.Controls.Add(this.clickablePanel3);
-            this.Controls.Add(this.clickablePanel2);
-            this.Controls.Add(this.clickablePanel1);
+            this.Controls.Add(this.pnlMainArea);
+            this.Controls.Add(this.pnlLeftSelection);
+            this.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "NewMainHandler";
             this.Text = "NewMainHandler";
             this.Load += new System.EventHandler(this.NewMainHandler_Load);
+            this.pnlLeftSelection.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -109,6 +152,8 @@
         private Custom_Controls.ClickablePanel clickablePanel3;
         private Custom_Controls.ClickablePanel clickablePanel4;
         private Custom_Controls.ClickablePanel clickablePanel5;
+        private System.Windows.Forms.Panel pnlLeftSelection;
+        private System.Windows.Forms.Panel pnlMainArea;
 
     }
 }
