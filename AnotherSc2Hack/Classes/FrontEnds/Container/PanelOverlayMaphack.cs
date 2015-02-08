7e35d74362ba149b7fcb4ae4083a04db71d681eb
bellaPatricia <bpatriciaella@yahoo.com>
2015-02-08 15:01:30 +0100

    Finished MaphackFilter. Now it's implemented like the old one.

--- AnotherSc2Hack/Classes/FrontEnds/Container/PanelOverlayMaphack.cs
+++ AnotherSc2Hack/Classes/FrontEnds/Container/PanelOverlayMaphack.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿/**
+ * This class is a container for the entire Maphack (overlay)- controls.
+ * Basically, it holds other containers and a few controls to manage the 
+ * maphack filter.
+ * 
+ * The maphack filter is implemented here and so is it's logic.
+ * Basically, you add a unit to the filter, the unit's ID get's listed into
+ * the listview and the information (unitId & color) get updated.
+ * 
+ * This works instantly.
+ * 
+ * 
+ * Author: bellaPatricia
+ * Date: 08 - February - 2015
+ * */
+
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Data;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using AnotherSc2Hack.Classes.ExtensionMethods;
 using AnotherSc2Hack.Classes.FrontEnds.MainHandler;
@@ -15,22 +26,27 @@ namespace AnotherSc2Hack.Classes.FrontEnds.Container
 {
     public partial class PanelOverlayMaphack : UserControl
     {
-        public Color SelectedUnitColor { get; private set; }
+        #region Getter/Setter
+
         public Dictionary<PredefinedData.UnitId, Color> LUnitFilter { get; private set; }
-        
+
+        #endregion
+
+        #region Constructor
 
         public PanelOverlayMaphack()
         {
             InitializeComponent();
 
             LUnitFilter = new Dictionary<PredefinedData.UnitId, Color>();
-            SelectedUnitColor = Color.Red;
         }
 
+        #endregion
+
+        #region Event Methods
         private void icbMaphackBasicsUnitSelection_SelectedIndexChanged(object sender, EventArgs e)
         {
             var unit = (ImageComboItem)icbMaphackBasicsUnitSelection.SelectedItem;
-
             AddUnit(unit.UnitId);
         }
 
@@ -40,8 +56,6 @@ namespace AnotherSc2Hack.Classes.FrontEnds.Container
 
             cl.ShowDialog();
 
-            SelectedUnitColor = cl.Color;
-
             foreach (ListViewItem selectedItem in lstvMaphackBasicsUnitFilter.SelectedItems)
             {
                 var id =
@@ -49,10 +63,45 @@ namespace AnotherSc2Hack.Classes.FrontEnds.Container
                     Enum.Parse(typeof(PredefinedData.UnitId), selectedItem.Text);
 
                 LUnitFilter[id] = cl.Color;
+                AddUnitToSettings(id, cl.Color);
             }
 
             RefreshListview();
         }
+
+        private void lstvMaphackBasicsUnitFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstvMaphackBasicsUnitFilter.SelectedIndices.Count != 1)
+                return;
+
+            var id =
+                (PredefinedData.UnitId)
+                    Enum.Parse(typeof(PredefinedData.UnitId), lstvMaphackBasicsUnitFilter.SelectedItems[0].Text);
+
+            if (LUnitFilter.ContainsKey(id))
+                btnMaphackBasicsUnitColor.BackColor = LUnitFilter[id];
+        }
+
+        private void cmsListviewContext_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if (e.ClickedItem.Text.Contains("Remove"))
+            {
+                RemoveSelectedItems();
+            }
+        }
+
+        private void lstvMaphackBasicsUnitFilter_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Delete)
+                return;
+
+            RemoveSelectedItems();
+        }
+
+        #endregion
+
+        #region Public Methods
+
 
         public void RefreshListview()
         {
@@ -74,17 +123,6 @@ namespace AnotherSc2Hack.Classes.FrontEnds.Container
             }
 
             lstvMaphackBasicsUnitFilter.Columns[0].Width = -2;
-
-            
-           
-            SyncPreferences();
-        }
-
-        private void SyncPreferences()
-        {
-            var myParent = (NewMainHandler)this.FindParent(String.Empty, typeof(NewMainHandler));
-            
-
         }
 
         public void AddUnitsToListview()
@@ -106,6 +144,45 @@ namespace AnotherSc2Hack.Classes.FrontEnds.Container
             }
 
             RefreshListview();
+        }
+
+
+
+        #endregion
+
+        #region Private Methods
+
+        private void AddUnitToSettings(PredefinedData.UnitId unitId, Color clColor)
+        {
+            var myParent = (NewMainHandler)this.FindParent(String.Empty, typeof(NewMainHandler));
+
+            var index = myParent.PSettings.MaphackUnitIds.FindIndex(x => x == unitId);
+
+
+            if (index == -1)
+            {
+                myParent.PSettings.MaphackUnitIds.Add(unitId);
+                myParent.PSettings.MaphackUnitColors.Add(clColor);
+            }
+
+            else
+            {
+                myParent.PSettings.MaphackUnitIds[index] = unitId;
+                myParent.PSettings.MaphackUnitColors[index] = clColor;
+            }
+        }
+
+        private void RemoveUnitFromSettings(PredefinedData.UnitId unitId)
+        {
+            var myParent = (NewMainHandler)this.FindParent(String.Empty, typeof(NewMainHandler));
+
+            var index = myParent.PSettings.MaphackUnitIds.FindIndex(x => x == unitId);
+
+            if (index == -1)
+                return;
+
+            myParent.PSettings.MaphackUnitIds.RemoveAt(index);
+            myParent.PSettings.MaphackUnitColors.RemoveAt(index);
         }
 
         private void AddUnit(PredefinedData.UnitId unitId)
@@ -133,28 +210,8 @@ namespace AnotherSc2Hack.Classes.FrontEnds.Container
             var cl = new Color().GetRandomColor();
             LUnitFilter.Add(unitId, cl);
 
-          AddUnitsToListview();
-        }
-
-        private void lstvMaphackBasicsUnitFilter_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (lstvMaphackBasicsUnitFilter.SelectedIndices.Count != 1)
-                return;
-
-            var id =
-                (PredefinedData.UnitId)
-                    Enum.Parse(typeof (PredefinedData.UnitId), lstvMaphackBasicsUnitFilter.SelectedItems[0].Text);
-
-            if (LUnitFilter.ContainsKey(id))
-                btnMaphackBasicsUnitColor.BackColor = LUnitFilter[id];
-        }
-
-        private void cmsListviewContext_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-        {
-            if (e.ClickedItem.Text.Contains("Remove"))
-            {
-                RemoveSelectedItems();
-            }
+            AddUnitsToListview();
+            AddUnitToSettings(unitId, cl);
         }
 
         private void RemoveSelectedItems()
@@ -163,20 +220,15 @@ namespace AnotherSc2Hack.Classes.FrontEnds.Container
             {
                 var id =
                 (PredefinedData.UnitId)
-                    Enum.Parse(typeof (PredefinedData.UnitId), selectedItem.Text);
+                    Enum.Parse(typeof(PredefinedData.UnitId), selectedItem.Text);
 
                 LUnitFilter.Remove(id);
+                RemoveUnitFromSettings(id);
             }
 
-           RefreshListview();
+            RefreshListview();
         }
 
-        private void lstvMaphackBasicsUnitFilter_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode != Keys.Delete)
-                return;
-
-            RemoveSelectedItems();
-        }
+        #endregion
     }
 }
