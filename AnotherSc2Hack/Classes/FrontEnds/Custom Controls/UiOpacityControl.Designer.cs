dff6c0d81b169f2d0a90756302d0b718eadf562f
bellaPatricia <bpatriciaella@yahoo.com>
2015-06-10 22:01:18 +0200

    Restore Opacity control

---
+++ AnotherSc2Hack/Classes/FrontEnds/Custom Controls/UiOpacityControl.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace AnotherSc2Hack.Classes.FrontEnds
+{
+    partial class UiOpacityControl
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOpacity = new System.Windows.Forms.Label();
+            this.tbOpacity = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.tbOpacity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblOpacity
+            // 
+            this.lblOpacity.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblOpacity.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblOpacity.Location = new System.Drawing.Point(-3, 35);
+            this.lblOpacity.Name = "lblOpacity";
+            this.lblOpacity.Size = new System.Drawing.Size(153, 13);
+            this.lblOpacity.TabIndex = 14;
+            this.lblOpacity.Text = "Blubb";
+            // 
+            // tbOpacity
+            // 
+            this.tbOpacity.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbOpacity.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.tbOpacity.Location = new System.Drawing.Point(0, 3);
+            this.tbOpacity.Maximum = 100;
+            this.tbOpacity.Name = "tbOpacity";
+            this.tbOpacity.Size = new System.Drawing.Size(147, 45);
+            this.tbOpacity.TabIndex = 13;
+            this.tbOpacity.Scroll += new System.EventHandler(this.tbOpacity_Scroll);
+            this.tbOpacity.ValueChanged += new System.EventHandler(this.tbOpacity_ValueChanged);
+            // 
+            // UiOpacityControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblOpacity);
+            this.Controls.Add(this.tbOpacity);
+            this.Name = "UiOpacityControl";
+            this.Size = new System.Drawing.Size(150, 53);
+            ((System.ComponentModel.ISupportInitialize)(this.tbOpacity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label lblOpacity;
+        public System.Windows.Forms.TrackBar tbOpacity;
+    }
+}
