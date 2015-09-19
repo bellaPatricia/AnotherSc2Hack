ab7ceb7309cde2afda85935c62f6326cbfc5fe86
bellaPatricia <bpatriciaella@yahoo.com>
2015-09-19 22:23:33 +0200

    Add basic structure for the GameInfo rewrite
    Basically, there's a static class which holds the properties to the needed data
    Then there's a bunch of threads taking care of filling of those properties.
    This runs forever and only shuts when the application will be closed.
    
    The filling requires the _correct_ offsets and a reference to the SC2 process.
    Using that, there's a dedicated thread for each needed variable making the
    filling more independent and split between cores. Thus: More efficient for one
    property.
    
    Not sure about the overhead though.
    
    The reason for the rewrite is kind of logical: There is only one instance that
    grabs the information from SC2. Currently, I pass the references to those
    variables around - a huge overhead and bad design at it's core.
    A static class exists only one time per application. So there's a huge
    convenient reason; logic.
    
    The drawback is the lack of plugin support. You have to send those data to the
    plugin by hand.

---
+++ AnotherSc2Hack/Classes/DataStructures/Game/Game.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using PredefinedTypes;
+
+namespace AnotherSc2Hack.Classes.DataStructures.Game
+{
+    public static class Game
+    {
+        private static bool _bInitialized;
+
+        private static Gameinformation _gameinformation;
+        private static List<Player> _player;
+        private static List<Unit> _unit;
+        private static List<Selection> _selection;
+        private static List<Groups> _group;
+        private static Map _map;
+
+        public static Gameinformation Gameinformation
+        {
+            get
+            {
+                OnAccessCall();
+                return _gameinformation;
+            }
+            set { _gameinformation = value; }
+        }
+
+        public static List<Player> Player
+        {
+            get
+            {
+                OnAccessCall();
+                return _player;
+            }
+            set { _player = value; }
+        }
+
+        public static List<Unit> Unit
+        {
+            get
+            {
+                OnAccessCall();
+                return _unit;
+            }
+            set { _unit = value; }
+        }
+
+        public static List<Selection> Selection
+        {
+            get
+            {
+                OnAccessCall();
+                return _selection;
+            }
+            set { _selection = value; }
+        }
+
+        public static List<Groups> Group
+        {
+            get
+            {
+                OnAccessCall();
+                return _group;
+            }
+            set { _group = value; }
+        }
+
+        public static Map Map
+        {
+            get
+            {
+                OnAccessCall();
+                return _map;
+            }
+            set { _map = value; }
+        }
+
+        private static void OnAccessCall()
+        {
+            Initialize();
+        }
+
+        private static void Initialize()
+        {
+            if (_bInitialized)
+                return;
+
+            //Initialize with class
+            Mind.Initialize();
+            _bInitialized = true;
+        }
+    }
+}
---
+++ AnotherSc2Hack/Classes/DataStructures/Game/Mind.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AnotherSc2Hack.Classes.DataStructures.Game
+{
+    public static class Mind
+    {
+        private static bool _bMindActive;
+        private static int _sleepTime = 100;
+
+        private static readonly List<Thread> LWorkers = new List<Thread>(); 
+
+        public static void Initialize()
+        {
+            LWorkers.Add(new Thread(PlayerWorker) {Name = "Worker: Player"});
+
+            LWorkers.ForEach(worker => worker.Start(worker));
+        }
+
+        private static void PlayerWorker(object sender)
+        {
+            var currentWorker = sender as Thread;
+            if (currentWorker == null)
+                return;
+
+            while (_bMindActive)
+            {
+                Thread.Sleep(_sleepTime);
+
+
+            }
+        }
+    }
+}
